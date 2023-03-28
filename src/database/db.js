66ee295ead0d78b2db2e44390acb2ef0a1a5c373
GeroWalther <gero.walther@gmail.com>
2023-03-29 00:54:01 +0200

    backend implemented

--- src/database/db.js
+++ src/database/db.js
@@ -7,7 +7,9 @@ const client = new MongoClient(process.env.MONGODB_URL);
 
 const database = client.db("test");
 const products = database.collection("products");
+const orders = database.collection("orders");
 
 module.exports = {
   products,
+  orders,
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const productRoutes = require("./router/productRoutes");
+const orderRoutes = require("./router/orderRoutes");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
+app.use(bodyParser.json());
 app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 
 app.get("/", (req, res) => {
   res.send("<h2>Hello world Nike Server!</h2>");
